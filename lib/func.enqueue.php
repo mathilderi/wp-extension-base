a266bc9d8565fd5021765fb8109021ad52524e99
Grégoire Noyelle <hello@gregoirenoyelle.com>
2016-12-01 12:10:16 +0100

    Correct file path for back and front
    the file func.enqueue.php is in a sub folder, we should use dirname(__FILE__) and not __FILE__
    For more information see the codex article: https://codex.wordpress.org/Function_Reference/plugins_url

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -10,14 +10,14 @@
 //* enqueue style front
 add_action('wp_enqueue_scripts','gn_styles_front', 99);
 function gn_styles_front() {
-	wp_register_style('styles-projet-front', plugins_url( '/css/styles-projet-front.css', __FILE__), array(), '1.0', 'all' );
+	wp_register_style('styles-projet-front', plugins_url( 'css/styles-projet-front.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-front');
 }
 
 //* enqueue script front
 add_action('wp_enqueue_scripts','gn_scripts_front');
 function gn_scripts_front() {
-	wp_register_script('scripts-projet-front',plugins_url( '/js/scripts-projet-front.js', __FILE__), array('jquery'),'1.0',true);
+	wp_register_script('scripts-projet-front',plugins_url( 'js/scripts-projet-front.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-front');
 }
 
@@ -28,13 +28,13 @@ function gn_scripts_front() {
 //* enqueue style back
 add_action('admin_enqueue_scripts','gn_styles_back', 99);
 function gn_styles_back() {
-	wp_register_style('styles-projet-back', plugins_url( '/css/styles-projet-back.css', __FILE__), array(), '1.0', 'all' );
+	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-back');
 }
 
 //* enqueue script back
 add_action('admin_enqueue_scripts','gn_scripts_back');
 function gn_scripts_back() {
-	wp_register_script('scripts-projet-back',plugins_url( '/js/scripts-projet-back.js', __FILE__), array('jquery'),'1.0',true);
+	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-back');
 }
