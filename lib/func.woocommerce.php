9fe60e3a2877a7eb9dfc3390074cbdc823e750e6
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-04-07 11:11:48 +0200

    Ajout d’une condition pour vérifier si WooCommerce est actif.

--- lib/func.woocommerce.php
+++ lib/func.woocommerce.php
@@ -1,2 +1,16 @@
 <?php
 // Pour changer le comportement de WooCommerce.
+
+if ( class_exists( 'WooCommerce' ) ) {
+  // code qui fonctionne avec WooCommerce
+
+} else {
+	// Notice dans le back-office au moment de la désactivation de WooCommerce
+	add_action('admin_notices','gnwoo_warning_woocommerce_missing');
+	function gnwoo_warning_woocommerce_missing() {
+	   $output = '<div id="my-custom-warning" class="error fade">';
+	   $output .= '<p><strong>Attention</strong>, ce site ne fonctionne pas sans l\'extension <strong>WooCommerce</strong>. Merci d\'activer cette extension</a>.</p>';
+	   $output .= '</div>';
+	   echo $output;
+	 }
+}
