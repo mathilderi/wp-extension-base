3dfb13d65e0d2031dcb28e987457989db8fd0a51
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-04-07 11:20:40 +0200

    Changement du hook pour le front et modif des préfixes de fonction

--- lib/func.woocommerce.php
+++ lib/func.woocommerce.php
@@ -6,15 +6,15 @@ if ( class_exists( 'WooCommerce' ) ) {
 
 	//* FRONT
 	// Ajout de texte avant les vignettes
-	add_action( 'woocommerce_before_shop_loop_item_title', 'gn_contenu_avant_vignette_2', 10);
-	function gn_contenu_avant_vignette_2() {
-		echo '<div>Avant les vignettes depuis le plugin</div>';
+	add_action( 'woocommerce_before_shop_loop', 'gnwooc2_contenu_avant_vignette_2', 10);
+	function gnwooc2_contenu_avant_vignette_2() {
+		echo '<h4 style="color:red;">Texte ajouté depuis le plugin Options pour WooCommerce</h4>';
 	}
 
 } else {
 	// Notice dans le back-office au moment de la désactivation de WooCommerce
-	add_action('admin_notices','gnwoo_warning_woocommerce_missing');
-	function gnwoo_warning_woocommerce_missing() {
+	add_action('admin_notices','gnwooc2_warning_woocommerce_missing');
+	function gnwooc2_warning_woocommerce_missing() {
 		$output = '<div id="my-custom-warning" class="error fade">';
 		$output .= '<p><strong>Attention</strong>, ce site ne fonctionne pas sans l\'extension <strong>WooCommerce</strong>. Merci d\'activer cette extension</a>.</p>';
 		$output .= '</div>';
