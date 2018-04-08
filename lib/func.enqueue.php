5134c25c9d9d85f8e5ba21ec8374de6cc7e265d4
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-04-08 06:24:59 +0200

    Suppression des hooks (commentaires) inutilisés et modication nom style css

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -10,12 +10,12 @@
 //* enqueue style front
 add_action('wp_enqueue_scripts','gnwooc2_styles_front', 99);
 function gnwooc2_styles_front() {
-	wp_register_style('styles-projet-front', plugins_url( 'css/styles-projet-front.css', dirname(__FILE__) ), array(), '1.0', 'all' );
-	wp_enqueue_style('styles-projet-front');
+	wp_register_style('gn-woocom-css', plugins_url( 'css/gn-woocom.css', dirname(__FILE__) ), array(), '1.0', 'all' );
+	wp_enqueue_style('gn-woocom-css');
 }
 
 //* enqueue script front
-add_action('wp_enqueue_scripts','gnwooc2_scripts_front');
+// add_action('wp_enqueue_scripts','gnwooc2_scripts_front');
 function gnwooc2_scripts_front() {
 	wp_register_script('scripts-projet-front',plugins_url( 'js/scripts-projet-front.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-front');
@@ -26,14 +26,14 @@ function gnwooc2_scripts_front() {
 **************************/
 
 //* enqueue style back
-add_action('admin_enqueue_scripts','gnwooc2_styles_back', 99);
+// add_action('admin_enqueue_scripts','gnwooc2_styles_back', 99);
 function gnwooc2_styles_back() {
 	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-back');
 }
 
 //* enqueue script back
-add_action('admin_enqueue_scripts','gnwooc2_scripts_back');
+// add_action('admin_enqueue_scripts','gnwooc2_scripts_back');
 function gnwooc2_scripts_back() {
 	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-back');
