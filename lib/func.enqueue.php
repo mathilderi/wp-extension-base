99295d5373fdd0626ee0fbc6d772a280056c7bd6
Unknown <mathirivoire@gmail.com>
2018-04-11 14:09:46 +0200

    Changement préfixe et désactivation requêtes inutiles

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -8,15 +8,15 @@
 **************************/
 
 //* enqueue style front
-add_action('wp_enqueue_scripts','gn_styles_front', 99);
-function gn_styles_front() {
+add_action('wp_enqueue_scripts','woocom2_styles_front', 99);
+function woocom2_styles_front() {
 	wp_register_style('styles-projet-front', plugins_url( 'css/styles-projet-front.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-front');
 }
 
 //* enqueue script front
-add_action('wp_enqueue_scripts','gn_scripts_front');
-function gn_scripts_front() {
+add_action('wp_enqueue_scripts','woocom2_scripts_front');
+function woocom2_scripts_front() {
 	wp_register_script('scripts-projet-front',plugins_url( 'js/scripts-projet-front.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-front');
 }
@@ -26,15 +26,15 @@ function gn_scripts_front() {
 **************************/
 
 //* enqueue style back
-add_action('admin_enqueue_scripts','gn_styles_back', 99);
-function gn_styles_back() {
-	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
-	wp_enqueue_style('styles-projet-back');
-}
+//add_action('admin_enqueue_scripts','woocom2_styles_back', 99);
+//function woocom2_styles_back() {
+//	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
+//	wp_enqueue_style('styles-projet-back');
+//}
 
 //* enqueue script back
-add_action('admin_enqueue_scripts','gn_scripts_back');
-function gn_scripts_back() {
-	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
-	wp_enqueue_script('scripts-projet-back');
-}
+//add_action('admin_enqueue_scripts','woocom2_scripts_back');
+//function woocom2_scripts_back() {
+//	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
+//	wp_enqueue_script('scripts-projet-back');
+//}
